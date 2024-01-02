b9c52caf0f559a339d2274c77463e178bf0511a6
Leo Bazan <leo.bazan.x@gmail.com>
2024-01-02 13:03:06 -0300

    UI: add devise views + boostrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root to: "home#dashboard"
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
   resources :banks
   resources :providers do
